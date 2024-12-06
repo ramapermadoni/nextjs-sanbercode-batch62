582e40f461871e1a12635dcf4e28c7ebf5ea936c
Rama Permadoni <ramaprogramming@gmail.com>
2024-12-06 21:19:51 +0700

    Tugas - Hari 5

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import Layout from "@/layout";
+
+
+export default function Profile() {
+    return (
+        <>
+            <Layout>
+                <p>Profile</p>
+            </Layout>
+        </>
+    );
+}
