46856605fe56805e94d34632ccd7244c09dac3ac
Rama Permadoni <ramaprogramming@gmail.com>
2024-12-12 22:55:52 +0700

    Tugas - Hari 9

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 
 export default function Main() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err))
+  }, [])
   return (
     <>
       <Layout metaTitle={"Home"} metaDescription={"Ini adalah halaman utama website"}>
