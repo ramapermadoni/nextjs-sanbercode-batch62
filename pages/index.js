0b7e9de94169c97322e80ba3ca4a72d6ae028b64
Rama Permadoni <ramaprogramming@gmail.com>
2024-12-14 07:38:08 +0700

    Tugas - Hari 10

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-import Layout from "@/layout";
+
+import Image from "next/image";
 import { useEffect } from "react";
+
+import dynamic from "next/dynamic";
+const LayoutDynamic = dynamic(() => import("@/layout"), { ssr: false });
 
 
 export default function Main() {
@@ -11,9 +15,19 @@ export default function Main() {
   }, [])
   return (
     <>
-      <Layout metaTitle={"Home"} metaDescription={"Ini adalah halaman utama website"}>
+      <LayoutDynamic metaTitle={"Home"} metaDescription={"Ini adalah halaman utama website"}>
         <p>Home</p>
-      </Layout>
+        <div style={{ position: "relative", height: "200px", maxHeight: "200px", width: "200px" }}>
+          <Image src="/nextjs.png" alt="Next.js Logo" fill style={{ objectFit: "contain" }} />
+        </div>
+
+        {/* ini paling recomended, tapi tidak merusak ratio meskipun definisi height dan width nya sama*/}
+        <Image src="/nextjs.png" alt="Next.js Logo" height={200} width={200} />
+
+
+        <img
+          src="/nextjs.png" style={{ width: "200px" }} alt="Next.js Logo" />
+      </LayoutDynamic>
     </>
   );
 }
